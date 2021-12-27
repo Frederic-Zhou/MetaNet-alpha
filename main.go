06938c2ef4f9db77f963114556e4d7b0f7f408aa
Frederic <zetachow@163.com>
2021-12-27 23:12:53 +0800

    修改代码结构

--- main.go
+++ main.go
@@ -22,22 +22,20 @@ func main() {
 		os.Exit(0)
 	}
 
-	client, err := node.GetClient()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
-	}
-
-	ctx := context.Background()
-
 	go func() {
 		if err := node.Start(); err != nil {
 			fmt.Println("over with error:", err)
 		}
 	}()
 
+	client, err := node.GetClient()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+
 	for {
-		client.BroadcastTxCommit(ctx, []byte("abc="+string(time.Now().Second())))
+		client.BroadcastTxCommit(context.Background(), []byte("abc="+string(time.Now().Second())))
 		time.Sleep(10 * time.Second)
 	}
 
