c71fd4279b4e5171619623ea492f265f0832e7e0
Frederic <zetachow@163.com>
2022-03-12 01:25:48 +0800

    实现服务端功能

--- NAT2P/client/main.go
+++ NAT2P/client/main.go
@@ -1,41 +1,102 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
 )
 
-func main() {
-	// 建立服务
-	listen, err := net.DialUDP("udp", nil, &net.UDPAddr{
-		IP:   net.IPv4(1, 14, 102, 100),
-		Port: 9090,
-	})
+var dialer, listener *net.UDPConn
+
+type peer struct {
+	Addr    string `json:"addr"`
+	Network string `json:"network"`
+}
+
+type peerInfo struct {
+	Addr    string          `json:"addr"`
+	Network string          `json:"network"`
+	Peers   map[string]peer `json:"peers"`
+}
+
+func dial2server() (pi peerInfo, err error) {
+	dialer, err = net.DialUDP("udp",
+		&net.UDPAddr{
+			IP:   net.IPv4(0, 0, 0, 0),
+			Port: 1234,
+		},
+		&net.UDPAddr{
+			IP:   net.IPv4(0, 0, 0, 0),
+			Port: 9090,
+		})
 	if err != nil {
 		fmt.Printf("listen udp server error:%v\n", err)
 	}
-	defer listen.Close()
+	defer dialer.Close()
 
 	fmt.Println("will send")
 	// 发送数据
 	sendData := []byte("Hello server")
-	_, err = listen.Write(sendData) // 发送数据
+	_, err = dialer.Write(sendData) // 发送数据
 	if err != nil {
 		fmt.Println("发送数据失败，err:", err)
 		return
 	}
 
-	fmt.Println("sended")
-
 	// 接收数据
 	data := make([]byte, 4096)
-	n, remoteAddr, err := listen.ReadFromUDP(data) // 接收数据
+	n := 0
+	n, _, err = dialer.ReadFromUDP(data) // 接收数据
 	if err != nil {
 		fmt.Println("接收数据失败，err:", err)
 		return
 	}
 
-	fmt.Println("readed")
-	fmt.Printf("recv:%v addr:%v count:%v\n", string(data[:n]), remoteAddr, n)
-	fmt.Println(listen.LocalAddr().String())
+	// fmt.Println(string(data[:n]))
+
+	err = json.Unmarshal(data[:n], &pi)
+	if err != nil {
+		fmt.Println("数据转换失败，err:", err)
+		return
+	}
+
+	return
+}
+
+func listen4peer() {
+	// 建立 udp 服务器
+	listener, err := net.ListenUDP("udp", &net.UDPAddr{
+		IP:   net.IPv4(0, 0, 0, 0),
+		Port: 1234,
+	})
+	if err != nil {
+		fmt.Printf("listen failed error:%v\n", err)
+		return
+	}
+	defer listener.Close() // 使用完关闭服务
+
+	for {
+		// 接收数据
+		var data [4096]byte
+		n, addr, err := listener.ReadFromUDP(data[:])
+		if err != nil {
+			fmt.Printf("read data error:%v\n", err)
+			return
+		}
+
+		fmt.Printf("addr:%v\t count:%v\t data:%v\n", addr, n, string(data[:n]))
+
+		// 发送数据
+		_, err = listener.WriteToUDP([]byte("ok"), addr)
+		if err != nil {
+			fmt.Printf("send data error:%v\n", err)
+			return
+		}
+	}
+}
+
+func main() {
+	pi, err := dial2server()
+
+	fmt.Println(pi, err)
 }
--- NAT2P/server/main.go
+++ NAT2P/server/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"net"
+	"sync"
 )
 
+var peersMap = sync.Map{}
+
+type peer struct {
+	Addr    string `json:"addr"`
+	Network string `json:"network"`
+}
+
 func main() {
+	udpServer()
+	// tcpServer()
+}
+
+func udpServer() {
 	// 建立 udp 服务器
 	listen, err := net.ListenUDP("udp", &net.UDPAddr{
 		IP:   net.IPv4(0, 0, 0, 0),
@@ -20,18 +35,87 @@ func main() {
 	for {
 		// 接收数据
 		var data [1024]byte
+		var id string
 		n, addr, err := listen.ReadFromUDP(data[:])
 		if err != nil {
 			fmt.Printf("read data error:%v\n", err)
 			return
 		}
+
+		id = string(data[:n])
 		fmt.Printf("addr:%v\t count:%v\t data:%v\n", addr, n, string(data[:n]))
-		fmt.Println(addr.String())
+
+		peersMap.Store(id, peer{Addr: addr.String(), Network: addr.Network()})
+
+		peers := map[string]peer{}
+		peersMap.Range(func(k interface{}, v interface{}) bool {
+			peers[k.(string)] = v.(peer)
+			return true
+		})
+		responsedata, err := json.Marshal(map[string]interface{}{
+			"addr":    addr.String(),
+			"network": addr.Network(),
+			"peers":   peers,
+		})
+
+		fmt.Println(string(responsedata))
+
+		if err != nil {
+			fmt.Printf("response data error:%v\n", err)
+			return
+		}
+
 		// 发送数据
-		_, err = listen.WriteToUDP(data[:n], addr)
+		_, err = listen.WriteToUDP(responsedata, addr)
 		if err != nil {
 			fmt.Printf("send data error:%v\n", err)
 			return
+		}
+	}
+}
+
+func tcpServer() {
+	// 建立 tcp 服务
+	listen, err := net.Listen("tcp", "0.0.0.0:9090")
+	if err != nil {
+		fmt.Printf("listen failed, err:%v\n", err)
+		return
+	}
+
+	for {
+		// 等待客户端建立连接
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Printf("accept failed, err:%v\n", err)
+			continue
+		}
+		// 启动一个单独的 goroutine 去处理连接
+		go process(conn)
+	}
+}
+
+func process(conn net.Conn) {
+	// 处理完关闭连接
+	defer conn.Close()
+
+	// 针对当前连接做发送和接受操作
+	for {
+		reader := bufio.NewReader(conn)
+		var buf [128]byte
+		n, err := reader.Read(buf[:])
+		if err != nil {
+			fmt.Printf("read from conn failed, err:%v\n", err)
+			break
+		}
+
+		recv := string(buf[:n])
+		fmt.Printf("收到的数据：%v\n", recv)
+
+		// 将接受到的数据返回给客户端
+		_, err = conn.Write([]byte("ok"))
+		if err != nil {
+			fmt.Printf("write from conn failed, err:%v\n", err)
+			break
 		}
 	}
 }
